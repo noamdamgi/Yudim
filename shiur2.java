a4963a352ee01b91caac27971c511fe4bbd89136
noamdamgi <144588321+noamdamgi@users.noreply.github.com>
2023-12-24 10:18:20 +0200

    Update shiur2.java

--- shiur2.java
+++ shiur2.java
@@ -1,1 +1,61 @@
+public class ND {
+
+	public static void main(String[] args) {
+		// ex1
+		System.out.println("ex 1______________________");
+		int num=0, num2=3, num3=4;
+		System.out.println(num);
+		System.out.println(num2);
+		System.out.println(num3);
+		// ex2
+		System.out.println("ex 2______________________");
+		int num4=6;
+		float num5=9.4f;
+		System.out.println(num4+"   "+num5);
+		//ex3
+		System.out.println("ex3________________________");
+		char str1='n';
+		char str2='o';
+		char str3='a';
+		char str4='m';
+		System.out.print(str1);
+		System.out.print(str2);
+		System.out.print(str3);
+		System.out.println(str4);
+		//ex4
+		System.out.println("ex4________________________");
+		int num6 = 100;
+		int num7 =99;
+		System.out.println(num6+" is bigger than "+num7);
+		//ex5  
+		System.out.println("ex5________________________");
+		int num8 = 5;
+		int num9 = 10;
+		int num10 = 15;
+		float score =(num8+num9+num10)/3;
+		System.out.println("the score is "+score);
+		//ex6
+		System.out.println("ex6________________________");
+		int a = 3;
+	    int b = 4;
+	    double c =(a * a) + (b * b);
+	    System.out.println("The hypotenuse of the right-angled triangle is: " + c);
+	    //7
+	    System.out.println("ex7________________________");
+	    int x=3;
+	    int y=0;
+	    y=2*x+5;
+	    System.out.println("y="+ y);
+	    //8
+	    System.out.println("ex8________________________");
+	    String str5 = "I like to study computer science";
+	    String str6 = "and therefore ready to invest";
+	    String str7 = "hours a day for practice";
+	    int num11=7;
+	    System.out.println(str5+" "+str6+" "+str7+" "+num11);
+	    }
+		
+
+	}
+
 
